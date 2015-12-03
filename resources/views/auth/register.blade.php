9a62c6d534994541f8a19027944ce6bcc2dda628
Sameque Santana <samer@alunos.uniararas.br>
2015-12-03 18:56:18 -0200

    <% %>

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,29 @@
-@extends('templates.default')
+<!-- resources/views/auth/register.blade.php -->
 
-@section('content')
+<form method="POST" action="/auth/register">
+    {!! csrf_field() !!}
 
-    @include('templates.registerUser')
+    <div>
+        Name
+        <input type="text" name="name" value="<% old('name') %>">
+    </div>
 
-@stop
+    <div>
+        Email
+        <input type="email" name="email" value="<% old('email') %>">
+    </div>
+
+    <div>
+        Password
+        <input type="password" name="password">
+    </div>
+
+    <div>
+        Confirm Password
+        <input type="password" name="password_confirmation">
+    </div>
+
+    <div>
+        <button type="submit">Register</button>
+    </div>
+</form>
