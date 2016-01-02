a4dfbcab68075b68811e1a58ac769597b1c574cf
Sameque <samer@alunos.uniararas.br>
2016-01-02 01:50:27 -0200

    update install

--- install.bat
+++ install.bat
@@ -1,8 +1,8 @@
 @echo off
-//composer install
 php artisan migrate:install
 php artisan migrate
-pause
+php artisan db:seed
 copy UTL\BladeCompiler.php vendor\laravel\framework\src\Illuminate\View\Compilers
-echo "Instalacao concluida"
+copy UTL\.htaccess ..\
+echo Instalacao concluida
 pause
