a80e4928a662c26edc4282b308cd17c8136ec53a
Sameque Santana <samer@alunos.uniararas.br>
2016-01-06 01:05:45 -0200

    Update URL repository

--- app/config/constants.php
+++ app/config/constants.php
@@ -1,8 +1,10 @@
 <?php
 
 define('ADMIN', 'administrator');
+//SPOJ
 define("URL_USER_SPOJ", 'http://br.spoj.com/users/');
 define("URL_PROBLEMS_SPOJ", 'http://br.spoj.com/problems/');
+define("URL_PROBLEMS__USER_SPOJ", 'http://br.spoj.com/status/');
 define("URL_USER_SPOJ_VALIDATE", 'http://br.spoj.com/users/');
 define("URL_SPOJ_RANKING", 'http://br.spoj.com/users/');
 
