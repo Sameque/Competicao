83e9798009c5add12af683dbe85046d78fcdc84b
Sameque Santana <samer@alunos.uniararas.br>
2016-03-19 12:27:30 -0300

    DateTime timeElapsed

--- app/Libraries/DateTime/DateTime.php
+++ app/Libraries/DateTime/DateTime.php
@@ -11,4 +11,17 @@ namespace App\Libraries\DateTime;
 
 class DateTime {
 
+
+    public  function  timeElapsed( $timeInit, $timeEnd){
+
+        $timeInit = explode(':',$timeInit);
+        $timeEnd = explode(':',$timeEnd);
+
+        $timeInitMin = ($timeInit[0]*60)+$timeInit[1];
+        $timeEndMin = ($timeEnd[0]*60)+$timeEnd[1];
+
+        $elapsed = $timeEndMin - $timeInitMin;
+
+        return $elapsed;
+    }
 } 
