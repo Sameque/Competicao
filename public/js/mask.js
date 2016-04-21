014acbd21966ffcd5ca0a5a35c0f487a12dd6f65
Sameque Santana <samer@alunos.uniararas.br>
2016-04-21 23:01:12 +0000

    add jQuery maskedinput

--- public/js/mask.js
+++ public/js/mask.js
@@ -1,3 +1,11 @@
 /**
  * Created by root on 4/21/16.
  */
+
+$(document).ready(function(){
+    $('h2').fadeOut(2000);
+    //$('h1').fadeOut(2000);
+    $('#dateBegin').mask("99/99/9999");
+    $('#dateEnd').mask("99/99/9999");
+
+});
